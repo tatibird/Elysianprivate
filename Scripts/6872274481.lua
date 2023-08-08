2288e10de36390aa3ab86caeec26e88c52ee2e72
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-08-08 18:35:14 +0300

    HighJump + Fly

--- Scripts/6872274481.lua
+++ Scripts/6872274481.lua
@@ -6,6 +6,7 @@ local UserInputService = game:GetService("UserInputService")
 local Players = game.Players
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character
+local Humanoid = Character.Humanoid
 
 local KnitClient = debug.getupvalue(require(LocalPlayer.PlayerScripts.TS.knit).setup, 6)
 
@@ -139,4 +140,98 @@ GuiLibrary.MakeButton({
         end
  
 	end,
+})
+
+local highjumpforce = {Value = 25}
+GuiLibrary.MakeButton({
+	["Name"] = "HighJump",
+	["Window"] = "Utility",
+	["Function"] = function(v)
+		local highjumpval = v
+            if highjumpval then
+                LocalPlayer.Character.Humanoid:ChangeState("Jumping")
+                task.wait()
+                workspace.Gravity = 5
+                spawn(function()
+                    for i = 1, highjumpforce["Value"] do
+                        wait()
+                        if (not highjumpval) then return end
+                        LocalPlayer.Character.Humanoid:ChangeState("Jumping")
+                    end
+                end)
+                spawn(function()
+                    for i = 1, highjumpforce["Value"] / 28 do
+                        task.wait(0.1)
+                        LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Freefall)
+                        task.wait(0.1)
+                        LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                    end
+                end)
+                highjump:silentToggle()
+            else
+                workspace.Gravity = 196.19999694824
+                return
+            end
+       end
+})
+
+local flygravityb = {Value = 0}
+local flyspeedb = {Value = 23}
+local FlyStudTP = {Value = 5}
+local flyenabled = false
+
+local function flyLogic()
+    if not flyenabled then
+        return
+    end
+    
+    local character = LocalPlayer.Character
+    if not character then
+        return
+    end
+    
+    if not Humanoid or Humanoid.Health == 0 then
+        return
+    end
+    
+    local HumanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+    if not HumanoidRootPart then
+        return
+    end
+    
+    workspace.Gravity = flygravityb.Value
+    Humanoid.WalkSpeed = flyspeedb.Value
+    
+    local SpaceHeld = UserInputService:IsKeyDown(Enum.KeyCode.Space)
+    local ShiftHeld = UserInputService:IsKeyDown(Enum.KeyCode.LeftShift)
+    
+    if SpaceHeld then
+        HumanoidRootPart.CFrame = HumanoidRootPart.CFrame * CFrame.new(0, FlyStudTP.Value, 0)
+    end
+    
+    if ShiftHeld then
+        HumanoidRootPart.CFrame = HumanoidRootPart.CFrame * CFrame.new(0, -FlyStudTP.Value, 0)
+    end
+end
+
+GuiLibrary.MakeButton({
+    ["Name"] = "Fly",
+    ["Window"] = "Utility",
+    ["Function"] = function(v)
+        flyenabled = v
+        if flyenabled then
+            spawn(function()
+                while flyenabled do
+                    flyLogic()
+                    wait()
+                end
+            end)
+        else
+            if LocalPlayer.Character then
+                LocalPlayer.Character.Humanoid.WalkSpeed = flyspeedb.Value
+            end
+            workspace.Gravity = 196
+        end
+ 
+    end,
 })
