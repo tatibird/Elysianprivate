13c414b0dbe0e69ab6fdf4024a69a533dbb944e8
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-08-23 21:32:32 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2,13 +2,14 @@ repeat task.wait() until game:IsLoaded()
 
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 local LocalPlayer = game.Players.LocalPlayer
+local PlaceID = game.PlaceId or 123456789
 
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/tatibird/Elysianprivate/main/MainScript.lua'))()")
     end
 end)
-if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
+if PlaceID == 6872274481 or PlaceID == 8560631822 or PlaceID == 8444591321 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tatibird/Elysianprivate/main/Bedwars"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tatibird/Elysianprivate/main/Scripts/Universal.lua"))()
